1da72cf35e346a1c9032a396f20387cab445ec24
Doha0 <abu.jardoha@gmail.com>
2023-05-02 00:02:43 +0600

    header and footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Shared/Header/Header';
+import Footer from '../Components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
-        <div>
+        <div className='container mx-auto'>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
