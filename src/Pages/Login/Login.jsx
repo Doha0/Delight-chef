eb1724f81775b584a8ebb3084986577ccf60b357
Doha0 <abu.jardoha@gmail.com>
2023-05-04 23:21:06 +0600

    styles updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProvider';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Login = () => {
 
@@ -19,7 +21,7 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         //console.log(email, password);
-        
+
         setError('');
         signIn(email, password)
             .then(result => {
@@ -74,39 +76,40 @@ const Login = () => {
                         <h1 className="text-5xl my-4 font-bold">Login to Delight Chef!</h1>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handelLogin} className="card-body">
-                            <div className="form-control">
+                        <div className="card-body ">
+
+
+                            <form onSubmit={handelLogin} >
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='password' className="input input-bordered" required />
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="custom-button">Login</button>
+                                </div>
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <Link to='/sign-up' className="label-text-alt link link-hover">New to Delight Chef? Create An Account</Link>
                                 </label>
-                                <input type="email" name='email' className="input input-bordered" required />
+                            </form>
+                            <div className="flex flex-col w-full border-opacity-50">
+                                <div className="divider">OR</div>
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" name='password' className="input input-bordered" required />
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="custom-button">Login</button>
-                            </div>
-                            <label className="label">
-                                <Link to='/sign-up' className="label-text-alt link link-hover">New to Delight Chef? Create An Account</Link>
-                            </label>
-                        </form>
-                        <div className='card-body'>
-                            <hr />
-                            <p className='text-center '>or</p>
-                            <div className="form-control mt-6">
-                                <button onClick={handleGoogleSignIn} className="btn bg-white hover:bg-slate-200 text-black ">Sign-in with Google</button>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button onClick={handleGithubSignIn} className="btn bg-black hover:bg-slate-800 text-white">Sign-in with GitHub</button>
-                            </div>
-                            <label className="label">
+                            <button onClick={handleGoogleSignIn} className="btn bg-white hover:bg-slate-200 text-black "><FcGoogle size={28}></FcGoogle> <span className='pl-2'>Sign-in with Google</span></button>
+                            <button onClick={handleGithubSignIn} className="btn bg-black hover:bg-slate-800 text-white"><FaGithub size={28}></FaGithub> <span className='pl-2'>Sign-in with GitHub</span></button>
+                            <p className="label">
                                 <p className="text-red-700">{error} </p>
-                            </label>
+                            </p>
+
                         </div>
+
                     </div>
                 </div>
             </div>
